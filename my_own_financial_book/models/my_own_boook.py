03e31add2120e4609614f6a1adb447098a15a7b3
pavlo <pavlo.vyskrebentsev@gmail.com>
2026-01-12 00:13:57 +0200

    [FIX] Add expensive dependences

--- my_own_financial_book/models/my_own_boook.py
+++ my_own_financial_book/models/my_own_boook.py
@@ -27,12 +27,18 @@ class MyOwnFinbook(models.Model):
 
     month_should_paid = fields.Float("Monthly Planned Payment", digits=(16, 2), compute="_compute_month_should_paid")
 
+    this_month_paid = fields.Float("This Month Paid (Expensive Goals)", digits=(16, 2), compute="_compute_this_month_paid")
+
+    debt = fields.Float("Debt (Paid - Planned)", digits=(16, 2), compute="_compute_debt")
+
     expense_chart = fields.Char("Expense Chart", compute="_compute_expense_chart")
+
+    comment = fields.Text("Comment")
 
     @api.depends('fin_expenses.amount')
     def _compute_expense_sum(self):
         for rec in self:
-            rec.expense_sum = sum(rec.fin_expenses.mapped('amount'))
+            rec.expense_sum = -sum(rec.fin_expenses.mapped('amount'))
 
     @api.depends('fin_invoices.amount')
     def _compute_invoice_sum(self):
@@ -42,7 +48,7 @@ class MyOwnFinbook(models.Model):
     @api.depends('invoice_sum', 'expense_sum')
     def _compute_total(self):
         for rec in self:
-            rec.total = (rec.invoice_sum or 0.0) - (rec.expense_sum or 0.0)
+            rec.total = (rec.invoice_sum or 0.0) + (rec.expense_sum or 0.0)
 
     @api.depends('total', 'manual_amount')
     def _compute_total_with_manual(self):
@@ -66,6 +72,28 @@ class MyOwnFinbook(models.Model):
                 months = goal.months_planned or 1
                 total_monthly += (goal.price or 0.0) / months
             rec.month_should_paid = round(total_monthly, 2)
+
+    @api.depends('fin_expenses.amount', 'fin_expenses.category')
+    def _compute_this_month_paid(self):
+        for rec in self:
+            cat = self.env.ref('my_own_financial_book.expense_category_expensive_goals', raise_if_not_found=False)
+            if not cat:
+                rec.this_month_paid = 0.0
+                continue
+            expenses = rec.fin_expenses.filtered(lambda e: e.category and e.category.id == cat.id)
+            rec.this_month_paid = round(sum(expenses.mapped('amount')), 2)
+
+    @api.depends('this_month_paid', 'month_should_paid')
+    def _compute_debt(self):
+        for rec in self:
+            # base debt for the current record
+            base_debt = (rec.this_month_paid or 0.0) - (rec.month_should_paid or 0.0)
+            # sum base debts of all previous records by create_date
+            prev_recs = self.search([('create_date', '<', rec.create_date)]) if rec.create_date else self.browse()
+            prev_base_total = 0.0
+            for prev in prev_recs:
+                prev_base_total += (prev.this_month_paid or 0.0) - (prev.month_should_paid or 0.0)
+            rec.debt = round(base_debt + prev_base_total, 2)
 
     def create_invoice_wizard(self):
         return {
